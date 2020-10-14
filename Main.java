94682ebbe444ee10be8272d3902f821e1ea320c6
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-14 15:18:01 +0000

    asked the user for a number and told them how many 1s there is in that number

--- Main.java
+++ Main.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that shows the user how many 1's there are in their entered number
+ * @author Daniel Rahmani
  */
 public class Main {
+
+  public static int countOnes(int number){
+    // create a do while loop
+    do{
+      // get the mod of the numbers until it gets them all
+      number = number / 10;
+      // create and if statement
+      if(number == 1){
+        number = number + number;
+      }  
+    }while(number >= 10);
+    
+    
+    int countOnes = number;
+    return countOnes;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a number
+    System.out.println("Please enter a number");
+    int n = input.nextInt();
+    // calculate the number of ones there are
+    int answer = countOnes(n);
+    System.out.println(answer);
     
   }
 }
